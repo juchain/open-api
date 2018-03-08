9b5b3117c0fdae9f0ea643acf75bf92b9b496248
blockshine <mark.ma@blockshine.com>
2018-03-08 18:35:11 +0800

    update BaseController

--- src/main/java/com/blockshine/common/web/BaseController.java
+++ src/main/java/com/blockshine/common/web/BaseController.java
@@ -1,5 +1,6 @@
 package com.blockshine.common.web;
 
+import com.blockshine.common.constant.CodeConstant;
 import com.blockshine.common.exception.BusinessException;
 import com.blockshine.common.util.R;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,11 +15,20 @@ public abstract class BaseController {
 
 
     @ExceptionHandler
-    public R handleBDException(BusinessException e) {
-        R r = new R();
-        r.put("code", e.getCode());
-        r.put("msg", e.getMessage());
-        return r;
+    public R handleException(Exception e) {
+        if(e instanceof BusinessException){
+            BusinessException businessException = (BusinessException)e;
+            R r = new R();
+            r.put("code", businessException.getCode());
+            r.put("msg", businessException.getMessage());
+            return r;
+        }else{
+
+            R r = new R();
+            r.put("code", CodeConstant.INTERAL_ERROR);
+            r.put("msg", e.getMessage());
+            return r;
+        }
     }
 
 }
