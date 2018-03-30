78c0650c220c54b10afdc3ffbb67876cc2dd5bf8
allen.gu <allen.gu@blockshine.com>
2018-03-30 11:58:44 +0800

    数据上链相关接口

--- src/main/java/com/blockshine/api/domain/AddressDO.java
+++ src/main/java/com/blockshine/api/domain/AddressDO.java
@@ -21,7 +21,7 @@ public class AddressDO implements Serializable {
 	//状态 0:禁用，1:正常
 	private Integer status;
 	//应用id
-	private Long appId;
+	private String appId;
 	//链类型0-共有链 1-私有链
 	private Integer type;
 	//转出地址
@@ -79,19 +79,17 @@ public class AddressDO implements Serializable {
 	public Integer getStatus() {
 		return status;
 	}
-	/**
-	 * 设置：应用id
-	 */
-	public void setAppId(Long appId) {
-		this.appId = appId;
-	}
-	/**
-	 * 获取：应用id
-	 */
-	public Long getAppId() {
+
+	public String getAppId() {
 		return appId;
 	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
 	/**
+
 	 * 设置：链类型0-共有链 1-私有链
 	 */
 	public void setType(Integer type) {
